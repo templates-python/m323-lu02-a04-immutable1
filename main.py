477dad9ae9d336ab35a50698ca67f8654247f0c0
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-08-30 11:03:06 +0200

    done

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-students = None
+students = [
+    ('Alice', 20, 1),
+    ('Bob', 22, 2),
+    ('Charlie', 19, 3),
+]
 
 def print_students(student_list):
-    # Your code here
-    pass
+    print('Studenten Informationen:')
+    for student in student_list:
+        name, age, student_id = student
+        print(f'Name: {name}, Alter: {age}, ID: {student_id}')
 
 if __name__ == '__main__':
     print_students(students)
