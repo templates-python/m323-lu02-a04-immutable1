804bf92eb294cc072b8a23fc09fb1d506347cdf7
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:03:03 +0200

    Ran pyink on branch solution

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ students = [
     ('Charlie', 19, 3),
 ]
 
+
 def print_students(student_list):
     print('Studenten Informationen:')
     for student in student_list:
         name, age, student_id = student
         print(f'Name: {name}, Alter: {age}, ID: {student_id}')
+
 
 if __name__ == '__main__':
     print_students(students)
